d17528d469d20afb1ee51a31fe8f9cccb964e9aa
DRSUCCESS <57950989+DRSUCCESS@users.noreply.github.com>
2024-05-10 05:31:23 +0100

    upgrade

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cURL get body size
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Get the byte size of the HTTP response header for a given URL.
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
