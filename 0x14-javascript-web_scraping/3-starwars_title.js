54f5ab34ca1d5797158f31dd37c9755ae34cf35b
DRSUCCES <a4surprise@gmail.com>
2024-06-05 21:54:06 -0700

    perfect

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-
 request(url, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error('Error:', 'Failed to load page, status code:', response.statusCode);
-    return;
-  }
-  try {
-    const data = JSON.parse(body);
-    console.log(data.title);
-  } catch (parseError) {
-    console.error('Error:', 'Failed to parse JSON');
-  }
+  console.log(error || JSON.parse(body).title);
 });
-
